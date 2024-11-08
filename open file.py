0eace099b00b7510af2ce6aa0405e2afed0f2de5
AlexB-png <alexander.brankin.466@accesscreative.ac.uk>
2024-11-08 16:19:13 +0000

    done

--- open file.py
+++ open file.py
@@ -1,9 +1,21 @@
+error =0
 f= open("volunteer.txt","a")
 name= input("What is your name?")
-bags= int(input("How many correct bags have you made"))
-bad = int(input("How many mistakes?"))
-percent=str((bags/bad)) + "%"
-a = name +","+ str(bags) +","+ str(bad) +","+ str(percent)
-print(a)
-f.write(a + "\n")
-f.close()
+bags= input("How many correct bags have you made")
+try:
+    int(bags)
+except:
+    print("error")
+    error=1
+bad =input("How many mistakes?")
+try:
+    int(bad)
+except:
+    print("error")
+    error=1
+if error != 1:
+    percent=str((int(bags)/int(bad))) + "%"
+    a = name +","+ str(bags) +","+ str(bad) +","+ str(percent)
+    print(a)
+    f.write(a + "\n")
+    f.close()
